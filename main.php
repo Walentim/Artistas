f7b9412ef459f7a7660209c06f1b80d612eb6dbc
Walentim <108528046+Walentim@users.noreply.github.com>
2022-11-21 12:19:06 -0300

    codi

---
+++ main.php
@@ -0,0 +1,17 @@
+<!--
+####################################################################################################################
+########################################## MAIN ROCKXABA ARTISTAS ##################################################
+####################################################################################################################
+-->
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"> 
+</head> 
+<body class="main">
+        <div class="texto-main">
+            <h3>Faça seu Login de Artista<h3>
+            <h3>Ainda não é Artista Parceiro da nossa plataforma?<h3>
+        <div class="">
+
+    </body>
+</html>
